0763a11a5d7b033c54160ce8e47484da0893c1d2
NattapongNaksoot <139940383+NattapongNaksoot@users.noreply.github.com>
2023-09-27 12:14:06 +0700

    Unit 8

---
+++ server/src/app.js
@@ -0,0 +1,38 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let cors = require('cors')
+
+
+const  {sequelize}  = require('./models')   
+
+const config = require('./config/config')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+require('./route')(app)
+
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server belong to Nattapong')
+})
+
+app.get('/hello/:name', function (req, res) {
+    console.log('hello - ' + req.params.name)
+    res.send('say hello with ' + req.params.name)
+})
+
+let port = process.env.PORT || config.port
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
+})
+
+
+
+
+
